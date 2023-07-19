230549e4fcc5818c30132278e2b258748d35f9b0
UponTheSky <sailing.to.orbis@gmail.com>
2023-07-19 20:00:00 +0900

    cmake tutorial - step 2

--- code/Step2/MathFunctions/MathFunctions.cxx
+++ code/Step2/MathFunctions/MathFunctions.cxx
@@ -1,15 +1,21 @@
 #include "MathFunctions.h"
 
 // TODO 11: include cmath
+#include <cmath>
 
+#ifdef USE_MYMATH
 // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
 #include "mysqrt.h"
-
+#endif
 namespace mathfunctions {
 double sqrt(double x)
 {
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
-  // Otherwise, use std::sqrt.
+  #ifdef USE_MYMATH
   return detail::mysqrt(x);
+  #else
+  // Otherwise, use std::sqrt.
+  return std::sqrt(x);
+  #endif
 }
 }
---
+++ code/Step2_build/takeaways.md
@@ -0,0 +1,8 @@
+# Takeaways
+- how to link a library
+  - use a subdirectory and `CMakeLists.txt` inside it
+  - add a new link, and link it to the target(either an executable or a lib)
+  - you can link a lib to another lib
+
+- conditional compile
+  - add compile definitions to the target
