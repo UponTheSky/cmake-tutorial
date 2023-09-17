12fc8bfbee3603dc826a4d078c96f684881eaf7d
UponTheSky <sailing.to.orbis@gmail.com>
2023-09-17 14:26:01 +0900

    cmake tutorial: review step1

--- official_tutorial/Step1/tutorial.cxx
+++ official_tutorial/Step1/tutorial.cxx
@@ -3,16 +3,17 @@
 #include <iostream>
 #include <string>
 
-#include "TutorialConfig.h"
-
 // TODO 11: Include TutorialConfig.h
+#include "TutorialConfig.h"
 
 int main(int argc, char* argv[])
 {
   if (argc < 2) {
     // TODO 12: Create a print statement using Tutorial_VERSION_MAJOR
     //          and Tutorial_VERSION_MINOR
-    std::cout << "Tutorial version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
+    std::cout << argv[0] << "Version " << Tutorial_VERSION_MAJOR <<
+    "." << Tutorial_VERSION_MINOR << std::endl;
+
     std::cout << "Usage: " << argv[0] << " number" << std::endl;
     return 1;
   }
@@ -22,8 +23,5 @@ int main(int argc, char* argv[])
   const double inputValue = std::stod(argv[1]);
 
   // calculate square root
-  const double outputValue = sqrt(inputValue);
-  std::cout << "The square root of " << inputValue << " is " << outputValue
-            << std::endl;
   return 0;
 }
